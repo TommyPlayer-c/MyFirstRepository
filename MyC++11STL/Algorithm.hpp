baa73fbcea11ea83647cc382995cb23c743391c6
TommyPlayersr <75240008+TommyPlayer-c@users.noreply.github.com>
2020-12-22 08:13:06 +0800

    new

--- MyC++11STL/Algorithm.hpp
+++ MyC++11STL/Algorithm.hpp
@@ -47,7 +47,7 @@ InputIterator Myfind_if(InputIterator first, InputIterator last, UnaryPredicate 
 
 template <class InputIterator, class UnaryPredicate>
 typename iterator_traits<InputIterator>::difference_type
-Mycount_if(InputIterator first, InputIterator last, UnaryPredicate pred)
+count_if(InputIterator first, InputIterator last, UnaryPredicate pred)
 {
     typename iterator_traits<InputIterator>::difference_type ret = 0;
     while (first != last)
@@ -58,4 +58,23 @@ Mycount_if(InputIterator first, InputIterator last, UnaryPredicate pred)
     }
     return ret;
 }
+
+//unique函数属于STL中比较常用函数，它的功能是元素去重。即”删除”序列中所有相邻的重复元素(只保留一个)。
+//此处的删除，并不是真的删除，而是指重复元素的位置被不重复的元素给占领了(详细情况，下面会讲)。
+//由于它”删除”的是相邻的重复元素，所以在使用unique函数之前，一般都会将目标序列进行排序。
+template <class ForwardIterator>
+ForwardIterator Myunique(ForwardIterator first, ForwardIterator last)
+{
+    if (first == last)
+        return last;
+
+    ForwardIterator result = first;
+    while (++first != last)
+    {
+        if (!(*result == *first)) // or: if (!pred(*result,*first)) for version (2)
+            *(++result) = *first;
+    }
+    return ++result;
+}
+
 #endif
